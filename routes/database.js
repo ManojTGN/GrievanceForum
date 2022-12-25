0a8889b5c169474dc08db861d647491f3518be9e
ManojTGN <manoj.thunderviz@gmail.com>
2022-12-26 01:50:40 +0530

    update mysql2

--- routes/database.js
+++ routes/database.js
@@ -1,16 +1,12 @@
-var mysql=require('mysql');
+var mysql=require('mysql2');
 require('dotenv').config();
 
-var connection=mysql.createConnection({
-   host:process.env.DB_HOST,
-   user:'root',
-   /*port:6790,*/
-   password:process.env.DB_PASS,
-   database:'railway'
-});
-
-connection.connect(function(error){
-    if(error) throw error;
-});
+var connection = mysql.createPool({
+    user:'root',
+    port:6790,
+    host:process.env.DB_HOST,
+    password:process.env.DB_PASS,
+    database:'railway'
+})
 
 module.exports = connection; 
