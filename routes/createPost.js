2433e2ddc129e171cd601fa99fe34ee75717df62
ManojTGN <manoj.thunderviz@gmail.com>
2022-12-28 11:32:00 +0530

    update

--- routes/createPost.js
+++ routes/createPost.js
@@ -20,9 +20,9 @@ router.get("/",(request,response) => {
     }
 
     if (request.cookies['login'] in cookie){
-        let hash = crypto.createHash('md5').update((new Date()).toLocaleString()+request.cookies['login']['mail']+crypto.randomInt(1000)).digest('hex');
+        let hash = crypto.createHash('md5').update((new Date()).toLocaleString(undefined,{timeZone: 'Asia/Kolkata'})+request.cookies['login']['mail']+crypto.randomInt(1000)).digest('hex');
         connection.query(
-        "INSERT INTO `posts`(`id`, `mail`, `title`, `description`, `report`, `category`, `cat_name`, `visibility`, `anonymous`, `comment`, `draft`, `datetime`, `name`, `picture`) VALUES ('"+hash+"','"+cookie[request.cookies['login']]["mail"]+"','','','','0','','0','0','0','1','"+(new Date()).toLocaleDateString()+" "+(new Date()).toLocaleTimeString()+"','"+cookie[request.cookies['login']]["name"]+"','"+cookie[request.cookies['login']]["picture"]+"')",(err, result, fields)=>{
+        "INSERT INTO `posts`(`id`, `mail`, `title`, `description`, `report`, `category`, `cat_name`, `visibility`, `anonymous`, `comment`, `draft`, `datetime`, `name`, `picture`) VALUES ('"+hash+"','"+cookie[request.cookies['login']]["mail"]+"','','','','0','','0','0','0','1','"+(new Date()).toLocaleString(undefined,{timeZone: 'Asia/Kolkata'})+"','"+cookie[request.cookies['login']]["name"]+"','"+cookie[request.cookies['login']]["picture"]+"')",(err, result, fields)=>{
             response.redirect("../edit/"+hash);
         });
         connection.query("INSERT INTO `notifications`(`mail`, `title`, `message`, `button`, `icon`, `color`, `datetime`, `isread`) VALUES ('"+cookie[request.cookies['login']]["mail"]+"','Post Creation','Created A New Post, Saved In Draft.','"+'<a class="btn btn-sm btn-secondary" href="../edit/'+hash+'">Edit Post</a>'+"','fa-solid fa-file-pen','primary','"+(new Date()).toLocaleDateString()+" "+(new Date()).toLocaleTimeString()+"','0')",(err, result, fields)=>{});
